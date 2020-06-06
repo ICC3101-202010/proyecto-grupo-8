5fc58a5921812c5643299d2f60ad504bbb7e9c41
Cristobal Neira <62522973+cristobalneira@users.noreply.github.com>
2020-06-06 19:19:21 -0400

    hello

--- Proyecto POO/Consola Spotflix/Perfiles.cs
+++ Proyecto POO/Consola Spotflix/Perfiles.cs
@@ -17,7 +17,7 @@ namespace Proyecto_POO
         {
             InitializeComponent();
         }
-
+        Usuario Usuarioasociado;
         private void botonCrearPerfil_Click(object sender, EventArgs e)
         {
             Hide();
@@ -42,10 +42,38 @@ namespace Proyecto_POO
 
         private void Perfiles_Load(object sender, EventArgs e)
         {
-            foreach (var item in Spotflix.Lista_Perfiles) 
+            int b = 0;
+            foreach (var item in Spotflix.Lista_Perfiles)
             {
-               
+                if (item.Usuario_Asociado == Usuarioasociado)
+                {
+                    b += 1;
+                }
             }
+            if (b == 0)
+            {
+                botonPerfil1.Hide();
+                botonPerfil2.Hide();
+                botonPerfil3.Hide();
+                botonCrearPerfil.Location=new Point(247, 155);
+
+            }
+            else if (b == 1)
+            {
+                botonPerfil2.Hide();
+                botonPerfil3.Hide();
+                botonCrearPerfil.Location = new Point(247, 155);
+
+            }
+            else if (b == 2)
+            {
+                botonPerfil3.Hide();
+                botonCrearPerfil.Location = new Point(410, 155);
+            }
+            else
+            {
+            }
+
         }
     }
 }
