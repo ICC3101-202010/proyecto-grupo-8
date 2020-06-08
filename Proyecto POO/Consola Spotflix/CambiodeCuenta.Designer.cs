2d8e785bc2fcf5367ffc9bccc9c543d3b064ae1b
camilavillalobos1 <62117125+camilavillalobos1@users.noreply.github.com>
2020-06-07 22:55:17 -0400

    Cambio de Cuenta

---
+++ Proyecto POO/Consola Spotflix/CambiodeCuenta.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Consola_Spotflix
+{
+    partial class CambiodeCuenta
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Cambiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(31, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cambio de cuenta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label2.Location = new System.Drawing.Point(43, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Que desea cambiar?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(209, 140);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(209, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label3.Location = new System.Drawing.Point(43, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(209, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.Location = new System.Drawing.Point(209, 320);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(225, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Location = new System.Drawing.Point(209, 362);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(225, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.Location = new System.Drawing.Point(209, 404);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(225, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox6.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.Location = new System.Drawing.Point(209, 441);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(225, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label4.Location = new System.Drawing.Point(43, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label5.Location = new System.Drawing.Point(43, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label6.Location = new System.Drawing.Point(43, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label7.Location = new System.Drawing.Point(43, 405);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label8.Location = new System.Drawing.Point(43, 442);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.comboBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(440, 239);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(225, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // Cambiar
+            // 
+            this.Cambiar.Location = new System.Drawing.Point(527, 420);
+            this.Cambiar.Name = "Cambiar";
+            this.Cambiar.Size = new System.Drawing.Size(108, 38);
+            this.Cambiar.TabIndex = 17;
+            this.Cambiar.Text = "Cambiar";
+            this.Cambiar.UseVisualStyleBackColor = true;
+            this.Cambiar.Click += new System.EventHandler(this.Cambiar_Click);
+            // 
+            // CambiodeCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.Controls.Add(this.Cambiar);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CambiodeCuenta";
+            this.Size = new System.Drawing.Size(710, 503);
+            this.Load += new System.EventHandler(this.CambiodeCuenta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button Cambiar;
+    }
+}
---
+++ Proyecto POO/Consola Spotflix/CambiodeCuenta.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_POO;
+
+namespace Consola_Spotflix
+{
+    public partial class CambiodeCuenta : UserControl
+    {
+        public CambiodeCuenta()
+        {
+            InitializeComponent();
+        }
+
+        private void CambiodeCuenta_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Nombre de perfil");
+            comboBox1.Items.Add("Tipo de Perfil");
+            comboBox1.Items.Add("Contraseña");
+            comboBox1.Items.Add("Membresia");
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string opcion = (string)comboBox1.SelectedItem;
+            if (opcion == "Nombre de perfil")
+            {
+                label3.Text = "Nombre";
+                textBox2.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                label4.Hide();
+                label5.Hide();
+                label6.Hide();
+                label7.Hide();
+                label8.Hide();
+                comboBox2.Hide();
+                
+            }
+            else if (opcion == "Tipo de perfil")
+            {
+                label3.Text = "Tipo de perfil";
+                comboBox2.Items.Add("Publico");
+                comboBox2.Items.Add("Privado");
+                textBox2.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                label4.Hide();
+                label5.Hide();
+                label6.Hide();
+                label7.Hide();
+                label8.Hide();
+                
+            }
+            else if (opcion == "Contraseña")
+            {
+                label3.Text = "Contraseña antigua";
+                label4.Text = "Contraseña nueva";
+                textBox3.Hide();
+                textBox4.Hide();
+                textBox5.Hide();
+                textBox6.Hide();
+                label5.Hide();
+                label6.Hide();
+                label7.Hide();
+                label8.Hide();
+                comboBox2.Hide();
+                
+
+            }
+            else if (opcion == "Membresia")
+            {
+                textBox1.Hide();
+                textBox2.Hide();
+                label3.Text = "Membresia";
+                label5.Text = "Banco";
+                label6.Text = "Numero de tarjeta";
+                label7.Text = "Fecha Vencimiento";
+                label8.Text = "Codigo de Seguridad";
+                label4.Hide();
+                comboBox2.Items.Add("Gratis");
+                comboBox2.Items.Add("Premium");
+
+            }
+
+        }
+
+        private void Cambiar_Click(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "Nombre de perfil")
+            {
+                string nombreperfil = textBox1.Text;
+                foreach (Perfil p in Spotflix.Lista_Perfiles)
+                {
+                    if (p == Spotflix.perfilenlinea)
+                    {
+                        p.Nombre_perfil = nombreperfil;
+                    }
+                }
+
+            }
+            else if ((string)comboBox1.SelectedItem == "Tipo de perfil")
+            {
+                int tipo = 0;
+                if ((string)comboBox2.SelectedItem == "Publico") 
+                {
+                    tipo = 1;
+                }
+                else 
+                {
+                    tipo = 2;
+                }
+
+                foreach (Perfil p in Spotflix.Lista_Perfiles)
+                {
+                    if (p == Spotflix.perfilenlinea)
+                    {
+                        p.Tipo_de_Perfil = tipo;
+                    }
+                }
+
+            }
+            else if ((string)comboBox1.SelectedItem == "Contraseña")
+            {
+                string contraseña = textBox2.Text;
+
+                foreach (Usuario p in Spotflix.Lista_Usuarios)
+                {
+                    if (p == Spotflix.usuarioenlinea)
+                    {
+                        p.Contraseña = contraseña;
+                    }
+        
+
+                }
+
+            }
+            else if ((string)comboBox1.SelectedItem == "Membresia")
+            {
+                int membresia = 0;
+                if ((string)comboBox2.SelectedItem == "Gratis")
+                {
+                    membresia = 1;
+                    label5.Hide();
+                    label6.Hide();
+                    label7.Hide();
+                    label8.Hide();
+                }
+                else
+                {
+                    membresia = 2;
+                }
+                foreach (Usuario p in Spotflix.Lista_Usuarios)
+                {
+                    if (p == Spotflix.usuarioenlinea)
+                    {
+                        p.Tipo_de_Membresia = membresia;
+                    }
+                }
+
+            }
+        }
+    }
+}
