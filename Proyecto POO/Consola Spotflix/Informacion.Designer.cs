051d1bbdd949c171e01a7599b6e0fe4c17ac3f81
Cristobal Neira <62522973+cristobalneira@users.noreply.github.com>
2020-06-08 01:06:23 -0400

    Vayan a dormirse Porfavor

---
+++ Proyecto POO/Consola Spotflix/Informacion.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Consola_Spotflix
+{
+    partial class Informacion
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.UsPer = new System.Windows.Forms.Button();
+            this.Multimedia = new System.Windows.Forms.Button();
+            this.People = new System.Windows.Forms.Button();
+            this.Atras = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(138, 177);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(305, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(141, 179);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(305, 199);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(138, 177);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(305, 199);
+            this.listBox3.TabIndex = 2;
+            // 
+            // UsPer
+            // 
+            this.UsPer.Location = new System.Drawing.Point(60, 91);
+            this.UsPer.Name = "UsPer";
+            this.UsPer.Size = new System.Drawing.Size(169, 54);
+            this.UsPer.TabIndex = 3;
+            this.UsPer.Text = "button1";
+            this.UsPer.UseVisualStyleBackColor = true;
+            this.UsPer.Click += new System.EventHandler(this.UsPer_Click_1);
+            // 
+            // Multimedia
+            // 
+            this.Multimedia.Location = new System.Drawing.Point(235, 91);
+            this.Multimedia.Name = "Multimedia";
+            this.Multimedia.Size = new System.Drawing.Size(169, 54);
+            this.Multimedia.TabIndex = 4;
+            this.Multimedia.Text = "Multimedia";
+            this.Multimedia.UseVisualStyleBackColor = true;
+            this.Multimedia.Click += new System.EventHandler(this.Multimedia_Click_1);
+            // 
+            // People
+            // 
+            this.People.Location = new System.Drawing.Point(410, 91);
+            this.People.Name = "People";
+            this.People.Size = new System.Drawing.Size(169, 54);
+            this.People.TabIndex = 5;
+            this.People.Text = "Personas";
+            this.People.UseVisualStyleBackColor = true;
+            this.People.Click += new System.EventHandler(this.People_Click_1);
+            // 
+            // Atras
+            // 
+            this.Atras.Location = new System.Drawing.Point(476, 381);
+            this.Atras.Name = "Atras";
+            this.Atras.Size = new System.Drawing.Size(169, 54);
+            this.Atras.TabIndex = 6;
+            this.Atras.Text = "button4";
+            this.Atras.UseVisualStyleBackColor = true;
+            this.Atras.Click += new System.EventHandler(this.Atras_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(236, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Informacion";
+            // 
+            // Informacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Atras);
+            this.Controls.Add(this.People);
+            this.Controls.Add(this.Multimedia);
+            this.Controls.Add(this.UsPer);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Informacion";
+            this.Size = new System.Drawing.Size(687, 473);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button UsPer;
+        private System.Windows.Forms.Button Multimedia;
+        private System.Windows.Forms.Button People;
+        private System.Windows.Forms.Button Atras;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Proyecto POO/Consola Spotflix/Informacion.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_POO;
+using static Proyecto_POO.Perfil;
+using static Proyecto_POO.Persona;
+using static Proyecto_POO.AudioLibro;
+using static Proyecto_POO.Video;
+
+namespace Consola_Spotflix
+{
+    public partial class Informacion : UserControl
+    {
+        public Informacion()
+        {
+            InitializeComponent();
+        }
+
+        private void UsPer_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Show();
+            listBox2.Hide();
+            listBox3.Hide();
+            int a = 1;
+            listBox1.Items.Clear();
+            foreach (Usuario u in Spotflix.Lista_Usuarios)
+            {
+                listBox1.Items.Add(a.ToString() + ". Usuario:" + u.Nombre + " Email: " + u.Email + " Pais: " + u.Pais + " Membresia: " + (enum_TipoDeMembresia)u.Tipo_de_Membresia);
+                listBox1.Items.Add("Perfiles:");
+                foreach (Perfil p in Spotflix.Lista_Perfiles)
+                {
+                    if (p.Usuario_Asociado == u)
+                    {
+                        listBox1.Items.Add("Perfil: " + p.Nombre_perfil + " Tipo de perfil: " + (enum_TipoDePerfil)p.Tipo_de_Perfil + " Usuario Asociado: " + p.Usuario_Asociado.Nombre);
+                    }
+                }
+            }
+        }
+
+        private void People_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Hide();
+            listBox2.Show();
+            listBox3.Hide();
+
+            listBox2.Items.Clear();
+            int b = 1;
+            foreach (Persona p in Spotflix.Lista_Personas)
+            {
+                string profesion = "";
+                foreach (var item in p.Profesion)
+                {
+                    profesion += (enum_Profesion)item + " ";
+                }
+                listBox2.Items.Add(b.ToString() + ". Persona: " + p.Nombre_y_Apellido + "Nacionalidad: " + p.Nacionalidad + " Profesion: " + profesion);
+                b += 1;
+            }
+        }
+
+        private void Multimedia_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Hide();
+            listBox2.Hide();
+            listBox3.Show();
+            listBox3.Items.Clear();
+            int c = 1;
+            foreach (AudioLibro al in Spotflix.Lista_AudioLibros)
+            {
+                listBox3.Items.Add(c.ToString() + ". AudioLibro: " + al.Titulo + " Año: " + al.Año + " Genero:" + al.Genero + " Idioma: " + (enum_Idioma)al.Idioma + " Ranking; " + al.Ranking);
+                c += 1;
+            }
+            listBox3.Items.Add("------------------------------------------------------------------------");
+            c = 0;
+            foreach (Cancion al in Spotflix.Lista_Canciones)
+            {
+                string cantantes = "";
+                foreach (var item in al.Cantante)
+                {
+                    cantantes += item + " ";
+                }
+                c += 1;
+                listBox3.Items.Add(c.ToString() + ". Cancion: " + al.Titulo + " Año: " + al.Año + " Genero:" + al.Genero + " Cantante/s: " + cantantes + " Ranking; " + al.Ranking);
+            }
+            listBox3.Items.Add("------------------------------------------------------------------------");
+            c = 0;
+            foreach (Pelicula al in Spotflix.Lista_Peliculas)
+            {
+                c += 1;
+                string actores = "";
+                foreach (var item in al.Actor)
+                {
+                    actores += item + " ";
+                }
+                listBox3.Items.Add(c.ToString() + ". Pelicula: " + al.Titulo + " Año: " + al.Año + " Genero:" + al.Genero + " Actores : " + actores + " Ranking; " + al.Ranking);
+                c += 1;
+            }
+            listBox3.Items.Add("------------------------------------------------------------------------");
+            c = 1;
+            foreach (Podcast al in Spotflix.Lista_Podcasts)
+            {
+                string locutores = "";
+                foreach (var item in al.Locutor)
+                {
+                    locutores += item + " ";
+                }
+                listBox3.Items.Add(c.ToString() + ". AudioLibro: " + al.Titulo + " Año: " + al.Año + " Genero:" + al.Genero + " Locutor " + locutores + " Ranking; " + al.Ranking);
+                c += 1;
+            }
+            listBox3.Items.Add("------------------------------------------------------------------------");
+            c = 0;
+            foreach (Video al in Spotflix.Lista_Videos)
+            {
+                c += 1;
+                listBox3.Items.Add(c.ToString() + ". Video: " + al.Titulo + " Año: " + al.Año + " Genero:" + al.Genero + "Clasificacion: " + (enum_Clasificacion)al.Clasificacion + " Ranking; " + al.Ranking);
+
+            }
+        }
+
+        private void Atras_Click_1(object sender, EventArgs e)
+        {
+            Hide();
+            Registro.mm1.Show();
+        }
+    }
+}
