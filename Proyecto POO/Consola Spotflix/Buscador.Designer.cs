0ad427a86979747cdf741506fc76c79a7e11d393
camilavillalobos1 <62117125+camilavillalobos1@users.noreply.github.com>
2020-06-08 21:49:50 -0400

    Avance Buscador

--- Proyecto POO/Consola Spotflix/Buscador.Designer.cs
+++ Proyecto POO/Consola Spotflix/Buscador.Designer.cs
@@ -33,22 +33,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.atras = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(229, 117);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(349, 21);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
+            this.comboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(229, 144);
             this.comboBox2.Name = "comboBox2";
@@ -57,17 +63,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(595, 295);
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.button1.Location = new System.Drawing.Point(589, 304);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(206, 45);
             this.button1.TabIndex = 5;
             this.button1.Text = "Seguir";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
             this.label3.Location = new System.Drawing.Point(677, 255);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
@@ -75,24 +83,28 @@
             // 
             // comboBox3
             // 
+            this.comboBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(229, 171);
+            this.comboBox3.Location = new System.Drawing.Point(229, 384);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(349, 21);
             this.comboBox3.TabIndex = 7;
             // 
-            // button2
+            // atras
             // 
-            this.button2.Location = new System.Drawing.Point(57, 295);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(206, 45);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Atras";
-            this.button2.UseVisualStyleBackColor = true;
+            this.atras.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.atras.Location = new System.Drawing.Point(57, 295);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(206, 45);
+            this.atras.TabIndex = 8;
+            this.atras.Text = "Atras";
+            this.atras.UseVisualStyleBackColor = true;
+            this.atras.Click += new System.EventHandler(this.atras_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
             this.label1.Location = new System.Drawing.Point(162, 120);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
@@ -102,6 +114,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
             this.label2.Location = new System.Drawing.Point(162, 144);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
@@ -111,26 +124,59 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Menu;
             this.label4.Location = new System.Drawing.Point(162, 171);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "label4";
             // 
+            // comboBox4
+            // 
+            this.comboBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(229, 198);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(349, 21);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label5.Location = new System.Drawing.Point(162, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(229, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
+            this.textBox1.TabIndex = 14;
+            // 
             // Buscador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.atras);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.Menu;
             this.Name = "Buscador";
             this.Size = new System.Drawing.Size(844, 460);
             this.Load += new System.EventHandler(this.Buscador_Load);
@@ -145,9 +191,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button atras;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Proyecto POO/Consola Spotflix/Buscador.cs
+++ Proyecto POO/Consola Spotflix/Buscador.cs
@@ -13,6 +13,7 @@ namespace Consola_Spotflix
 {
     public partial class Buscador : UserControl
     {
+
         public Buscador()
         {
             InitializeComponent();
@@ -22,7 +23,14 @@ namespace Consola_Spotflix
             Show();
             if (tipo == "Perfil")
             {
-                label2.Hide();
+                label1.Text = "Accion";
+                comboBox1.Items.Add("Seguir");
+                comboBox1.Items.Add("Dejar de seguir");
+                comboBox3.Hide();
+                comboBox4.Hide();
+                label4.Hide();
+                label5.Hide();
+                label2.Text = "Perfiles"; ;
                 comboBox2.Hide();
                 foreach (var item in Spotflix.Lista_Perfiles)
                 {
@@ -30,31 +38,361 @@ namespace Consola_Spotflix
                     {
                         if (item != item2)
                         {
-                            comboBox1.Items.Add(item);
+                            comboBox2.Items.Add(item);
                         }
 
                     }
                 }
                 label3.Text = "Perfil seguido!";
             }
-            else if (tipo == "Multimedia")
+            else if (tipo == "Canciones")
             {
-                label1.Text = "Multimedia";
+                label1.Text = "Añadir a ";
+                comboBox1.Items.Add("Playlist");
+                comboBox1.Items.Add("En Cola");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Titulo");
+                comboBox2.Items.Add("Cantante");
+                comboBox2.Items.Add("Album");
+                comboBox2.Items.Add("Genero");
+                label4.Text = "Tipo de Categoria";
+                label5.Text = "Canciones";
+                int profesion = 0;
+                if (textBox1.Text == "Cantante")
+                {
+                    profesion = 1;
+                }
+
+
+                foreach (var item in Spotflix.Lista_Canciones)
+                {
+                    int i = 0;
+                    if ((string)comboBox2.SelectedItem == "Cantante")
+                    {
+                        foreach (var item1 in Spotflix.Lista_Personas)
+                        {
+                            if ((Persona.enum_Profesion)profesion == Persona.enum_Profesion.Cantante)
+                            {
+                                comboBox4.Items.Add(Spotflix.Lista_Podcasts[i - 1].Titulo);
+
+                            }
+                        }
+                        comboBox4.Items.Add(Spotflix.Lista_Canciones[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Album" && item.Album == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Canciones[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Genero" && item.Genero == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Canciones[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Titulo" && item.Titulo == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Canciones[i - 1].Titulo);
+                        i++;
+                    }
+
+                }
             }
-           
+            else if (tipo == "Videos")
+            {
+                label1.Text = "Añadir a ";
+                comboBox1.Items.Add("Playlist");
+                comboBox1.Items.Add("En Cola");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Titulo");
+                comboBox2.Items.Add("Genero");
+                comboBox2.Items.Add("Clasificacion");
+                label4.Text = "Tipo de Categoria";
+                label5.Text = "Videos";
+                int clasificacion = 0;
+
+                if (textBox1.Text == "AA")
+                {
+                    clasificacion = 1;
+                }
+                else if (textBox1.Text == "A")
+                {
+                    clasificacion = 2;
+                }
+                else if (textBox1.Text == "B")
+                {
+                    clasificacion = 3;
+                }
+                else if (textBox1.Text == "C")
+                {
+                    clasificacion = 4;
+                }
+                else if (textBox1.Text == "D")
+                {
+                    clasificacion = 5;
+                }
+
+                foreach (var item in Spotflix.Lista_Videos)
+                {
+                    int i = 0;
+                    if ((string)comboBox2.SelectedItem == "Titulo" && item.Titulo == textBox1.Text)
+                    {
+                        comboBox4.Items.Add( Spotflix.Lista_Videos[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Genero" && item.Genero == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Videos[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Clasificacion" && item.Clasificacion == clasificacion)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Videos[i - 1].Titulo);
+                        i++;
+                    }
+
+                }
+            }
+            else if (tipo == "Peliculas")
+            {
+                label1.Text = "Añadir a ";
+                comboBox1.Items.Add("Playlist");
+                comboBox1.Items.Add("En Cola");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Titulo");
+                comboBox2.Items.Add("Genero");
+                comboBox2.Items.Add("Clasificacion");
+                label4.Text = "Tipo de Categoria";
+                label5.Text = "Peliculas";
+                int clasificacion = 0;
+
+                if (textBox1.Text == "AA")
+                {
+                    clasificacion = 1;
+                }
+                else if (textBox1.Text == "A")
+                {
+                    clasificacion = 2;
+                }
+                else if (textBox1.Text == "B")
+                {
+                    clasificacion = 3;
+                }
+                else if (textBox1.Text == "C")
+                {
+                    clasificacion = 4;
+                }
+                else if (textBox1.Text == "D")
+                {
+                    clasificacion = 5;
+                }
+
+                foreach (var item in Spotflix.Lista_Peliculas)
+                {
+                    int i = 0;
+                    if ((string)comboBox2.SelectedItem == "Titulo" && item.Titulo == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Peliculas[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Genero" && item.Genero == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Peliculas[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Clasificacion" && item.Clasificacion == clasificacion)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Peliculas[i - 1].Titulo);
+                        i++;
+                    }
+
+                }
+            }
+            else if (tipo == "Podcasts")
+            {
+                label1.Text = "Añadir a ";
+                comboBox1.Items.Add("Playlist");
+                comboBox1.Items.Add("En Cola");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Titulo");
+                comboBox2.Items.Add("Genero");
+                comboBox2.Items.Add("Locutor");
+                label4.Text = "Tipo de Categoria";
+                label5.Text = "Podcasts";
+                int profesion = 0;
+
+                if (textBox1.Text == "Locutor")
+                {
+                    profesion = 5;
+                }
+
+
+                foreach (var item in Spotflix.Lista_Podcasts)
+                {
+                    int i = 0;
+                    if ((string)comboBox2.SelectedItem == "Titulo" && item.Titulo == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Podcasts[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Genero" && item.Genero == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Podcasts[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Locutor")
+                    {
+                        foreach (var item1 in Spotflix.Lista_Personas)
+                        {
+                            if ((Persona.enum_Profesion)profesion == Persona.enum_Profesion.Locutor)
+                            {
+                                comboBox4.Items.Add(Spotflix.Lista_Podcasts[i - 1].Titulo);
+                                
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+            else if (tipo == "Audiolibros")
+            {
+                label1.Text = "Añadir a ";
+                comboBox1.Items.Add("Playlist");
+                comboBox1.Items.Add("En Cola");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Titulo");
+                comboBox2.Items.Add("Genero");
+                comboBox2.Items.Add("Lector");
+                label4.Text = "Tipo de Categoria";
+                label5.Text = "Audiolibro";
+                int profesion = 0;
+
+                if (textBox1.Text == "Lector")
+                {
+                    profesion = 6;
+                }
+                foreach (var item in Spotflix.Lista_AudioLibros)
+                {
+                    int i = 0;
+                    if ((string)comboBox2.SelectedItem == "Titulo" && item.Titulo == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_AudioLibros[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Genero" && item.Genero == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_AudioLibros[i - 1].Titulo);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Locutor")
+                    {
+                        foreach (var item1 in Spotflix.Lista_Personas)
+                        {
+                            if ((Persona.enum_Profesion)profesion == Persona.enum_Profesion.Locutor)
+                            {
+                                comboBox4.Items.Add(Spotflix.Lista_AudioLibros[i - 1].Titulo);
+
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+
             else if (tipo == "Artista")
             {
+                label1.Text = "Accion";
+                comboBox1.Items.Add("Seguir");
+                comboBox1.Items.Add("Dejar de seguir");
+                label2.Text = "Categoria";
+                comboBox2.Items.Add("Nacionalidad");
+                comboBox2.Items.Add("Sexo");
+                comboBox2.Items.Add("Profesion");
+                comboBox2.Items.Add("Edad");
+                label4.Text = "Tipo de busqueda";
+                int sexo = 0;
+                if (textBox1.Text == "Mujer")
+                {
+                    sexo = 2;
+                }
+                else 
+                {
+                    sexo = 1;
+                }
+                int profesion = 0;
+                if (textBox1.Text == "Cantante")
+                {
+                    profesion = 1;
+                }
+                else if (textBox1.Text == "Actor")
+                {
+                    profesion = 3;
+                }
+                else if (textBox1.Text == "Locutor")
+                {
+                    profesion = 5;
+                }
+                else if (textBox1.Text == "Lector")
+                {
+                    profesion = 6;
+                }
+
+                
+
+                int i = 0;
+                foreach (var item in Spotflix.Lista_Personas)
+                {
+                    if ((string)comboBox2.SelectedItem == "Nacionalidad" && item.Nacionalidad == textBox1.Text)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Personas[i - 1].Nombre_y_Apellido);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Sexo" && item.Sexo == sexo)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Personas[i-1].Nombre_y_Apellido);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Profesion" && item.Profesion[0] == profesion)
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Personas[i - 1].Nombre_y_Apellido);
+                        i++;
+                    }
+                    else if ((string)comboBox2.SelectedItem == "Edad" && 2020 -item.Fecha_De_Nacimiento.Year == Convert.ToInt32(textBox1.Text))
+                    {
+                        comboBox4.Items.Add(Spotflix.Lista_Personas[i - 1].Nombre_y_Apellido);
+                        i++;
+                    }
+                }
 
             }
             else if (tipo == "Playlist")
             {
+                foreach (var item in Spotflix.Lista_Playlists)
+                {
+                    comboBox2.Items.Add(item);
+                }
 
             }
             
         }
         private void Buscador_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            Hide();
         }
     }
 }
